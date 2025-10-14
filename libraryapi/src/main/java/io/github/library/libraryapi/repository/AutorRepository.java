d3f2ecabf71cdd21946485026d4e94925a43d5fb
Victor <victorneves596@gmail.com>
2025-10-14 13:48:35 -0300

    reatualizando na pasta Spring

---
+++ libraryapi/src/main/java/io/github/library/libraryapi/repository/AutorRepository.java
@@ -0,0 +1,9 @@
+package io.github.library.libraryapi.repository;
+
+import io.github.library.libraryapi.model.Autor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface AutorRepository extends JpaRepository<Autor, UUID> {
+}
