f9b4f360fb71677899d36f1eddd970b8831416e4
Victor <victorneves596@gmail.com>
2025-10-17 21:18:09 -0300

    Mensagem do commit

--- Spring/libraryapi/src/main/java/io/github/library/libraryapi/repository/AutorRepository.java
+++
@@ -1,9 +0,0 @@
-package io.github.library.libraryapi.repository;
-
-import io.github.library.libraryapi.model.Autor;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface AutorRepository extends JpaRepository<Autor, UUID> {
-}
